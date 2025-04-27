5d5d978aa99fb14a84b7baf8080c3121ff7a54ca
MiloAC1 <crabapplefarms@icloud.com>
2025-04-26 23:21:07 -0500

    Added Luminence calculations and oolor constraining

--- src/main.zig
+++ src/main.zig
@@ -58,6 +58,7 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
     const new_h = @divFloor(imgH, 8);
     const new_c = imgC;
     const new_pixel_count = new_w * new_h * new_c;
+    const pixel_count = imgW * imgH * imgC;
     
     var rimg: stb_image.Image = .{
         .width = @intCast(new_w),
@@ -67,7 +68,29 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
     };
     
     const slice: []u8 = try allocator.alloc(u8, new_pixel_count);
+    const linearized: []u8 = try allocator.alloc(u8, pixel_count);
 
+
+
+    for (0..imgW) |x| {
+        for (0..imgH) |y| {
+
+            const indexL = ((y * imgW + x) * imgC);
+
+            const r: f32 = @floatFromInt(imgP[indexL + 0]);
+            const g: f32 = @floatFromInt(imgP[indexL + 1]);
+            const b: f32 = @floatFromInt(imgP[indexL + 2]);
+            const vR: f32 = linearize(r/255.0);
+            const vG: f32 = linearize(g/255.0);
+            const vB: f32 = linearize(b/255.0);
+            const lum = vR * 0.2126 + vG * 0.7152 + vB * 0.0722;
+            const constrained = @floor(lum * 10) / 10;
+
+            for (0..imgC) |c| {
+                linearized[indexL + c] = @intFromFloat(constrained * 255);
+            }
+        }
+    }
     for (0..new_h) |y| {
         for(0..new_w) |x| {
             const originX = @divFloor(x * imgW, new_w);
@@ -75,12 +98,30 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
             const indexN = ((y * new_w + x ) * new_c);
             const indexO = ((originY * imgW + originX) * imgC);
 
+
             for (0..new_c) |c| {
-                slice[indexN + c] = imgP[indexO + c];
+                slice[indexN + c] = linearized[indexO + c];
             }
         }
     }
 
+
     rimg.pixels = if (slice.len != 0) &slice[0] else null;
     return rimg;
+}
+
+fn srgbize(c: f32) f32 {
+    if (c <= 0.0031308) {
+        return c * 12.92;
+    } else {
+        return std.math.pow(f32,c, 1.0/2.4) - 0.055;
+    }
+
+}
+fn linearize(c: f32) f32 {
+    if (c <= 0.04045) {
+        return c / 12.92;
+    } else {
+        return std.math.pow(f32,((c + 0.055)/1.055),2.4);
+    }
 }
