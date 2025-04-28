8f399a32a6316dda4bd18365051c3f9bbbd6f729
MiloAC1 <crabapplefarms@icloud.com>
2025-04-27 19:54:41 -0500

    Added Upscaling to the Downscaled image

--- src/main.zig
+++ src/main.zig
@@ -47,7 +47,6 @@ pub fn main() !void {
 }
 
 fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
-    
     const imgW: usize = @intCast(img.width);
     const imgH: usize = @intCast(img.height);
     const imgC: usize = @intCast(img.channels);
@@ -59,30 +58,31 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
     const new_c = imgC;
     const new_pixel_count = new_w * new_h * new_c;
     const pixel_count = imgW * imgH * imgC;
-    
+
     var rimg: stb_image.Image = .{
-        .width = @intCast(new_w),
-        .height = @intCast(new_h),
-        .channels = @intCast(new_c),
+        //        .width = @intCast(new_w),
+        //        .height = @intCast(new_h),
+        //        .channels = @intCast(new_c),
+        .width = img.width,
+        .height = img.height,
+        .channels = img.channels,
         .pixels = undefined,
     };
-    
+
     const slice: []u8 = try allocator.alloc(u8, new_pixel_count);
     const linearized: []u8 = try allocator.alloc(u8, pixel_count);
-
-
+    const upscale: []u8 = try allocator.alloc(u8, pixel_count);
 
     for (0..imgW) |x| {
         for (0..imgH) |y| {
-
             const indexL = ((y * imgW + x) * imgC);
 
             const r: f32 = @floatFromInt(imgP[indexL + 0]);
             const g: f32 = @floatFromInt(imgP[indexL + 1]);
             const b: f32 = @floatFromInt(imgP[indexL + 2]);
-            const vR: f32 = linearize(r/255.0);
-            const vG: f32 = linearize(g/255.0);
-            const vB: f32 = linearize(b/255.0);
+            const vR: f32 = linearize(r / 255.0);
+            const vG: f32 = linearize(g / 255.0);
+            const vB: f32 = linearize(b / 255.0);
             const lum = vR * 0.2126 + vG * 0.7152 + vB * 0.0722;
             const constrained = @floor(lum * 10) / 10;
 
@@ -92,21 +92,40 @@ fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
         }
     }
     for (0..new_h) |y| {
-        for(0..new_w) |x| {
+        for (0..new_w) |x| {
             const originX = @divFloor(x * imgW, new_w);
             const originY = @divFloor(y * imgH, new_h);
-            const indexN = ((y * new_w + x ) * new_c);
+            const indexN = ((y * new_w + x) * new_c);
             const indexO = ((originY * imgW + originX) * imgC);
-
 
             for (0..new_c) |c| {
                 slice[indexN + c] = linearized[indexO + c];
             }
         }
     }
+    for (0..new_h) |y| {
+        std.debug.print("y is {}\n", .{y});
+        for (0..new_w) |x| {
+            const oX = @divFloor(x * imgW, new_w);
+            const oY = @divFloor(y * imgH, new_h);
+            const index = ((y * new_w + x) * new_c);
+            std.debug.print("x is {}\n", .{x});
 
+            for (0..8) |h| {
+                for (0..8) |w| {
+                    const uX = oX + w;
+                    const uY = oY + h;
+                    const indexO = ((uY * imgW + uX) * imgC);
 
-    rimg.pixels = if (slice.len != 0) &slice[0] else null;
+                    for (0..new_c) |c| {
+                        upscale[indexO + c] = slice[index + c];
+                    }
+                }
+            }
+        }
+    }
+
+    rimg.pixels = if (upscale.len != 0) &upscale[0] else null;
     return rimg;
 }
 
@@ -114,14 +133,13 @@ fn srgbize(c: f32) f32 {
     if (c <= 0.0031308) {
         return c * 12.92;
     } else {
-        return std.math.pow(f32,c, 1.0/2.4) - 0.055;
+        return std.math.pow(f32, c, 1.0 / 2.4) - 0.055;
     }
-
 }
 fn linearize(c: f32) f32 {
     if (c <= 0.04045) {
         return c / 12.92;
     } else {
-        return std.math.pow(f32,((c + 0.055)/1.055),2.4);
+        return std.math.pow(f32, ((c + 0.055) / 1.055), 2.4);
     }
 }
