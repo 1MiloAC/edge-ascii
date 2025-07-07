a3ad22b879efc35cf7b6f14a6725905bbfaa62c0
miloa <miloa@miloas-MacBook-Air.local>
2025-07-06 21:33:49 -0500

    Removed cpu side image resizing

--- src/main.zig
+++ src/main.zig
@@ -525,30 +525,23 @@ pub fn main() !void {
 
         const ofilename = "wgpu.png";
 
+        defer if (img.pixels != null) {
+            stb_image.freeImage(&img);
+        };
+
+        std.debug.print("loaded image size: {?}x{?} with {?} channels\n", .{ img.width, img.height, img.channels });
+
+        if (img.pixels) |pixels| {
+            std.debug.print("image pixel data: {*}\n", .{pixels});
+        } else {
+            std.debug.print("image pixel data: (null)\n", .{});
+        }
+        std.debug.print("attempting to write image\n", .{});
+
         stb_image_write.writeImage(ofilename, &final_image);
-
         wgpu.wgpuBufferUnmap(copy_buffer);
+
     }
-
-    var rimg = try resize(alloc, img);
-
-    defer if (img.pixels != null) {
-        stb_image.freeImage(&img);
-    };
-
-    std.debug.print("loaded image size: {?}x{?} with {?} channels\n", .{ img.width, img.height, img.channels });
-
-    if (img.pixels) |pixels| {
-        std.debug.print("image pixel data: {*}\n", .{pixels});
-    } else {
-        std.debug.print("image pixel data: (null)\n", .{});
-    }
-    const rfilename = "test2.png";
-    const wfilename = "test.png";
-    std.debug.print("attempting to write image", .{});
-
-    stb_image_write.writeImage(wfilename, &img);
-    stb_image_write.writeImage(rfilename, &rimg);
 }
 fn createShadderModule(device: ?*wgpu.struct_WGPUDeviceImpl) ?*wgpu.struct_WGPUShaderModuleImpl {
     std.debug.print("Shader content: {any}\n", .{shader_wgsl});
@@ -777,107 +770,4 @@ fn cbufferinit(size: usize) wgpu.WGPUBuffer {
         .mappedAtCreation = wgpu.WGPUOptionalBool_False,
     };
     return wgpu.wgpuDeviceCreateBuffer(state.device, &desc);
-}
-
-fn resize(allocator: std.mem.Allocator, img: stb_image.Image) !stb_image.Image {
-    const imgW: usize = @intCast(img.width);
-    const imgH: usize = @intCast(img.height);
-    const imgC: usize = @intCast(img.channels);
-    const bufP: [*]u8 = @ptrCast(img.pixels.?);
-    const imgP: []u8 = bufP[0..(imgW * imgH * imgC)];
-
-    const new_w = @divFloor(imgW, 8);
-    const new_h = @divFloor(imgH, 8);
-    const new_c = imgC;
-    const new_pixel_count = new_w * new_h * new_c;
-    const pixel_count = imgW * imgH * imgC;
-
-    var rimg: stb_image.Image = .{
-        //        .width = @intCast(new_w),
-        //        .height = @intCast(new_h),
-        //        .channels = @intCast(new_c),
-        .width = img.width,
-        .height = img.height,
-        .channels = img.channels,
-        .pixels = undefined,
-    };
-
-    const slice: []u8 = try allocator.alloc(u8, new_pixel_count);
-    const upscale: []u8 = try allocator.alloc(u8, pixel_count);
-
-    const linear = try luminize(imgW, imgH, imgC, imgP, allocator);
-
-    for (0..new_w) |x| {
-        for (0..new_h) |y| {
-            const originX = @divFloor(x * imgW, new_w);
-            const originY = @divFloor(y * imgH, new_h);
-            const indexN = ((y * new_w + x) * new_c);
-            const indexO = ((originY * imgW + originX) * imgC);
-
-            for (0..new_c) |c| {
-                slice[indexN + c] = linear[indexO + c];
-            }
-        }
-    }
-    for (0..new_w) |x| {
-        for (0..new_h) |y| {
-            const oX = @divFloor(x * imgW, new_w);
-            const oY = @divFloor(y * imgH, new_h);
-            const index = ((y * new_w + x) * new_c);
-
-            for (0..8) |w| {
-                for (0..8) |h| {
-                    const uX = oX + w;
-                    const uY = oY + h;
-                    const indexO = ((uY * imgW + uX) * imgC);
-
-                    for (0..new_c) |c| {
-                        upscale[indexO + c] = slice[index + c];
-                    }
-                }
-            }
-        }
-    }
-
-    rimg.pixels = if (upscale.len != 0) &upscale[0] else null;
-    return rimg;
-}
-
-fn luminize(w: usize, h: usize, c: usize, p: []u8, alloc: std.mem.Allocator) ![]u8 {
-    const pc = w * h * c;
-    const linearized: []u8 = try alloc.alloc(u8, pc);
-    for (0..w) |x| {
-        for (0..h) |y| {
-            const indexL = ((y * w + x) * c);
-
-            const r: f32 = @floatFromInt(p[indexL + 0]);
-            const g: f32 = @floatFromInt(p[indexL + 1]);
-            const b: f32 = @floatFromInt(p[indexL + 2]);
-            const vR: f32 = linearize(r / 255.0);
-            const vG: f32 = linearize(g / 255.0);
-            const vB: f32 = linearize(b / 255.0);
-            const lum = vR * 0.2126 + vG * 0.7152 + vB * 0.0722;
-            const constrained = @round(srgbize(lum) * 9) / 9;
-
-            for (0..c) |i| {
-                linearized[indexL + i] = @intFromFloat(std.math.clamp(constrained * 255, 0, 255));
-            }
-        }
-    }
-    return linearized;
-}
-
-fn srgbize(c: f32) f32 {
-    if (c <= 0.0031308) {
-        return c * 12.92;
-    } else {
-        return std.math.pow(f32, c, 1.0 / 2.4) - 0.055;
-    }
-}
-fn linearize(c: f32) f32 {
-    if (c <= 0.04045) {
-        return c / 12.92;
-    } else {
-        return std.math.pow(f32, ((c + 0.055) / 1.055), 2.4);
-    }
 }
